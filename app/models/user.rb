6440e678eab55f15f65719977bbfcbca2e67b591
Dan Perata <DanPerata@gmail.com>
2013-04-23 17:01:45 -0600

    added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,13 @@ class User < ActiveRecord::Base
   def full_name
     first_name + " " + last_name
   end
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcassed_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcassed_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
+
 end
